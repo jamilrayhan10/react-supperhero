7e2bc21dfece012cc7c3967ea0807f340ef60650
jamilrayhan <rjamil336@gmail.com>
2021-09-26 17:21:24 +0600

    redmi

--- src/components/ProgrammarTeam/ProgrammarTeam.js
+++ src/components/ProgrammarTeam/ProgrammarTeam.js
@@ -32,6 +32,7 @@ const ProgrammarTeam = () => {
           </div>
         </div>
         <div className="col-lg-3">
+          {/* programmart summary  */}
           <ShowProgrammar showprogrammar={showprogrammar}></ShowProgrammar>
         </div>
       </div>
